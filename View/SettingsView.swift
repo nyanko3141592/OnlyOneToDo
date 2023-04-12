d365822aeaf61b4a5b568074005f48295839f393
nya3_neko2 <takahashinaoki521@gmail.com>
2023-04-12 20:07:44 +0900

    fix

--- View/SettingsView.swift
+++ View/SettingsView.swift
@@ -7,7 +7,7 @@ struct SettingsView: View {
         VStack {
             Form {
                 Section {
-                    Text("ToDo app for dementia).font(.title)
+                    Text("ToDo app for dementia").font(.title)
                     Text("My grandmother’s dementia has worsened since this year. After she eats lunch, she asks, “Have I had lunch yet?” and after taking a bath, she says, “I feel sick because I didn’t take a bath today.” She has been struggling with this issue. Normal to-do apps have too much information for her. What she needs to see is only one to-do task at a time, and it would be better if it is displayed in large letters. I believe that this app will improve her life.")
                     Spacer()
                 } header: {
